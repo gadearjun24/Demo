0878da0f75553de69d37544d4310f089d60fc17e
Arjun Gade <139616230+gadearjun24@users.noreply.github.com>
2024-10-06 16:28:35 +0000

    add cors

--- index.js
+++ index.js
@@ -3,12 +3,14 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const bodyParser = require('body-parser');
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to SQLite database
